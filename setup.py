74200a5b940dde43fc76fcd5e569f60326a34f37
Wenwei Huang <dut.hww@gmail.com>
2017-05-25 12:16:11 +0800

    Add type attribute

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ttsserver',
-    version="0.1.6",
+    version="0.1.7",
     packages=['ttsserver'],
     description=('Hanson Robotics TTS Server.'),
     url='https://github.com/hansonrobotics/ttsserver',
