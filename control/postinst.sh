6630e307e5f6378dc0bef8e6a219ff770005cabc
Wenwei Huang <dut.hww@gmail.com>
2018-07-11 11:45:42 +0800

    Fix pysptk package: the higher version breaks

--- control/postinst.sh
+++ control/postinst.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ -e /usr/bin/hr ]]; then
-    hr cmd pip2_install flask sox>=1.2.9 pysptk>=0.1.4 numpy scipy colorlog
+    hr cmd pip2_install flask sox>=1.2.9 numpy scipy pysptk==0.1.4 colorlog
 fi
