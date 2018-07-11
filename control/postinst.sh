aa1d7882756afd85ce5133fef9ada3a86e24c415
Wenwei Huang <dut.hww@gmail.com>
2018-07-11 12:57:54 +0800

    Add dependency

--- control/postinst.sh
+++ control/postinst.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ -e /usr/bin/hr ]]; then
-    hr cmd pip2_install flask sox>=1.2.9 numpy scipy pysptk==0.1.4 colorlog
+    hr cmd pip2_install flask sox>=1.2.9 numpy scipy pysptk==0.1.4 colorlog pinyin==0.2.5
 fi
